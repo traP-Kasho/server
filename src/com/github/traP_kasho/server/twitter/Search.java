e1a90b390049a7436ed0e16aaf4d01b76e1a24d0
RLook <example@example.rich>
2017-07-07 23:57:34 +0900

    自分のフォロワー内でのツイート検索を可能に

---
+++ src/com/github/traP_kasho/server/twitter/Search.java
@@ -0,0 +1,26 @@
+package com.github.traP_kasho.server.twitter;
+
+import twitter4j.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Search {
+    public static List<Status> search(String name) throws TwitterException {
+        final int MAX_VALUE = 10;
+        List<Status> statuses = new ArrayList<>();
+        Twitter twitter = AuthTwitter.getTwitter();
+        //このフィルター実は非公開だったりするので若干†ヤバ†がある
+        Query query = new Query(name + " filter:follows");
+        query.setCount(100);
+        QueryResult result;
+
+        int i = 1;
+        do {
+            result = twitter.search(query);
+            statuses.addAll(result.getTweets());
+            i++;
+        } while ((query = result.nextQuery()) != null && i <= MAX_VALUE);
+        return statuses;
+    }
+}
