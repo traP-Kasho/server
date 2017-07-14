c76282fc84727d28a216e31966f850d3dc7423d6
RLook <example@example.rich>
2017-07-14 16:09:36 +0900

    プロパティファイル管理の実装

--- src/com/github/traP_kasho/server/PropertyManager.java
+++ src/com/github/traP_kasho/server/PropertyManager.java
@@ -1,7 +1,19 @@
 package com.github.traP_kasho.server;
 
+import java.util.ResourceBundle;
+
 /**
  * Created by poispois on 2017/07/13.
  */
-public class Propertymanager {
+public final class PropertyManager {
+    private static final String PROPERTY_FILE_NAME = "settings";
+    private static ResourceBundle bundle;
+
+    static {
+        ResourceBundle bundle = ResourceBundle.getBundle(PROPERTY_FILE_NAME);
+    }
+
+    public static String getValue(String key) {
+        return bundle.getString(key);
+    }
 }
